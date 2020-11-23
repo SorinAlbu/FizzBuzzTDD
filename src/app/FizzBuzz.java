abba8020b4d46f298c9a53d45bce8adb0c2355a5
Sorin <albusorin28@gmail.com>
2020-11-23 15:56:25 +0200

    Update FizzBuzz.java

--- src/app/FizzBuzz.java
+++ src/app/FizzBuzz.java
@@ -6,10 +6,11 @@ import org.springframework.stereotype.Component;
 public class FizzBuzz {
     private static final String FIZZ = "Fizz";
     private static final String BUZZ = "Buzz";
+    private static final String ERROR_MSG = "Number must be greater than 0";
 
     public String computeFizzBuzz(int number) throws IllegalArgumentException{
         if (number <= 0) {
-            throw new IllegalArgumentException("Number must be greater than 0");
+            throw new IllegalArgumentException(ERROR_MSG);
         }
 
         boolean isThreeMultiple = number % 3 == 0;
